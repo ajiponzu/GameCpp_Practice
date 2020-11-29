e8d359e8c198927719d0da46685447a17e17601f
mita <cpjvmm@outlook.jp>
2020-11-29 10:31:20 +0900

    課題2開始

---
+++ GameCpp2/src/Component.h
@@ -0,0 +1,30 @@
+// ----------------------------------------------------------------
+// From Game Programming in C++ by Sanjay Madhav
+// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
+// 
+// Released under the BSD License
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+
+#pragma once
+
+using Actor = class Actor;
+
+class Component
+{
+public:
+	// Constructor
+	// (the lower the update order, the earlier the component updates)
+	Component(Actor* owner, int updateOrder = 100);
+	// Destructor
+	virtual ~Component();
+	// Update this component by delta time
+	virtual void Update(float deltaTime);
+
+	int GetUpdateOrder() const { return mUpdateOrder; }
+protected:
+	// Owning actor
+	Actor* mOwner;
+	// Update order of component
+	int mUpdateOrder;
+};
